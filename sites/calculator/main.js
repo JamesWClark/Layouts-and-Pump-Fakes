5e7db7682d2f42e7a3e6a07ee92046bd1f8a0dba
J.W. Clark <jwclark@rockhursths.edu>
2017-01-11 23:09:10 -0600

    Add some more layers to LCD and math functions

--- sites/calculator/main.js
+++ sites/calculator/main.js
@@ -1,10 +1,12 @@
 ;(function() {
     
     var memory;
+    var operator;
     
     var nextClear = false;
     
-    var lcd = document.getElementById('lcd');
+    var lcd = document.getElementById('main-panel');
+    var top = document.getElementById('top-panel');
     
     var buttons = document.getElementsByClassName('numeric');
     
@@ -24,22 +26,43 @@
         memory = null;
     };
     
-    document.getElementById('addition').onclick = function() {
-        if(!memory) {
-            memory = Number(lcd.innerHTML);
-            lcd.innerHTML = '';
-        } else {
-            lcd.innerHTML = Number(lcd.innerHTML) + memory;
-            memory = null;
-        }
-    };
-    
     document.getElementById('equals').onclick = function() {
         if(memory) {
-            lcd.innerHTML = Number(lcd.innerHTML) + memory;
+            lcd.innerHTML = getResult();
+            top.innerHTML = '';
             memory = null;
             nextClear = true;
         }
     };
+    
+    var operators = document.getElementsByClassName('operator');
+    
+    for(var i = 0; i < operators.length; i++) {
+        operators[i].onclick = function() {
+            operator = this.innerHTML;
+            if(!memory) {
+                memory = Number(lcd.innerHTML);
+                lcd.innerHTML = '';
+                top.innerHTML = Number(memory) + ' ' + operator;
+            } else {
+                memory = null;
+            }   
+        };
+    }
+    
+    function getResult() {
+        var a = memory;
+        var b = Number(lcd.innerHTML);
+        switch(operator) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case 'x':
+                return a * b;
+            case '÷':
+                return a / b;
+        }
+    }
     
 })();
